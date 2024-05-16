5383a498cd2a06f3e77cb54cc5a7b114c25f44d5
Mohammad Ehtisham <106554718+EhtishamSattar@users.noreply.github.com>
2024-05-16 23:09:59 +0500

    Find vs filter

--- Find-Filter.js
+++ Find-Filter.js
@@ -1,5 +1,8 @@
 const x = [1, 2, 3, 4, 5];
 
+// The filter() method returns a new array containing all elements of the original array that satisfy the provided testing function.
+// It iterates through the entire array and collects all elements that meet the condition.
+
 const y = x.filter(el => el%2 === 0);
 
 console.log("y is: ", y); // y is: [2, 4]
@@ -18,6 +21,9 @@ const emp = [
         empID: 103
     }
 ];
+
+// find() returns the first element that met the condition
+// it stops iterating if the element is found
 
 const res = emp.find(el => el.empID === 102);
 
