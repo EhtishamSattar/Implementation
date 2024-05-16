defc05b06de924bec1b6e54f983b19a8a8e661ce
Mohammad Ehtisham <106554718+EhtishamSattar@users.noreply.github.com>
2024-05-16 23:23:33 +0500

    Map vs Filter

--- forEach-Map.js
+++ forEach-Map.js
@@ -1,11 +1,37 @@
 const numbers = [1, 2, 3, 4, 5];
 
 // Using map()
+
+/*
+The map() method iterates over each element of the array and applies a provided function to each element.
+It returns a new array containing the results of applying the provided function to each element in the original array.
+The original array remains unchanged.
+
+Map vs Filter
+
+map() is used to transform each element of the array and produce a new array
+with the transformed values, while filter() is used to select elements from the
+array that satisfy a certain condition and produce a new array containing only those elements.
+Additionally, map() preserves the length of the array, while filter() may result in a
+shorter array if some elements are filtered out.
+
+*/
+  
 console.time('map');
 const doubledNumbersMap = numbers.map((num) => num * 2);
 console.timeEnd('map');
 
 // Using forEach()
+
+/*
+
+The forEach() method iterates over each element of the array and executes a provided function for each element.
+It does not return anything (returns undefined).
+It is typically used when you want to perform some action for each element in the array but don't need to produce
+a new array with the results.
+
+
+*/
 console.time('forEach');
 const doubledNumbersForEach = [];
 numbers.forEach((num)=>{
